86a90652a4415d4fab4f72fc34e9f71702b670e7
Your NameGRANMOAL <grecoangel135@gmail.com>
2023-11-07 12:01:37 -0600

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,7 +3,8 @@ git commit -m "Ultimo Commit"
 git push
 go build main.go
 del main.zip
-"C:\Program Files\7-Zip" a -tzip main.zip main
+Compress-Archive -Path .\main -DestinationPath main.zip
+
 
 
 
